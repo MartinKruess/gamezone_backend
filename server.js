8efadaa7e826904cb03ed6c4f75692ebcbe1a085
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2024-02-24 11:48:23 +0100

    http/s

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: 'https://raikuns-gamezone.netlify.app/' || "http://localhost:5173",
+        origin: 'https://raikuns-gamezone.netlify.app/' | "http://localhost:5173",
         accessControll: true,
         credentials: true
     }
